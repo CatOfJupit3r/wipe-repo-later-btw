24e84ad52af702b24aa6602d12e01c304dacf9ef
CatOfJupit3r <catofjupit3r@gmail.com>
2025-10-15 23:34:19 +0300

    feat: add assignment 1

---
+++ src/assignment-01/index.php
@@ -0,0 +1,97 @@
+<?php
+$group = "Група: КН-000";
+$developer = "Розробник: Прізвище Ім'я По батькові";
+$created = "Дата створення: 15.10.2025";
+$now = date("d.m.Y H:i");
+
+$amountInput = isset($_GET['amount']) ? $_GET['amount'] : '';
+$amount = '';
+$error = '';
+$resultClassic = '';
+$resultAlternative = '';
+
+if ($amountInput !== '') {
+    $amount = (int) $amountInput;
+
+    if ($amount < 1 || $amount > 999) {
+        $error = "Введіть число від 1 до 999";
+    } else {
+        $lastTwo = $amount % 100;
+        $last = $amount % 10;
+
+        $formClassic = "гривень";
+
+        if ($lastTwo >= 11 && $lastTwo <= 14) {
+            $formClassic = "гривень";
+        } else {
+            switch ($last) {
+                case 1:
+                    $formClassic = "гривня";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    $formClassic = "гривні";
+                    break;
+                default:
+                    $formClassic = "гривень";
+                    break;
+            }
+        }
+
+        // альтернативний синтаксис
+        $formAlt = "гривень";
+
+        if ($lastTwo >= 11 && $lastTwo <= 14) {
+            $formAlt = "гривень";
+        } else {
+            switch ($last):
+                case 1:
+                    $formAlt = "гривня";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    $formAlt = "гривні";
+                    break;
+                default:
+                    $formAlt = "гривень";
+                    break;
+            endswitch;
+        }
+
+        $resultClassic = $amount . " " . $formClassic;
+        $resultAlternative = $amount . " " . $formAlt;
+    }
+}
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Форми слова гривня</title>
+</head>
+<body>
+    <h1>Визначення правильної форми слова гривня</h1>
+
+    <form method="get">
+        <label>Число (1-999):</label>
+        <input type="number" name="amount" min="1" max="999" value="<?php echo htmlspecialchars($amountInput); ?>">
+        <button type="submit">Порахувати</button>
+    </form>
+
+    <?php if ($error !== '') { ?>
+        <p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
+    <?php } ?>
+
+    <?php if ($resultClassic !== '' && $resultAlternative !== '') { ?>
+        <p>Класичний switch: <?php echo htmlspecialchars($resultClassic); ?></p>
+        <p>Альтернативний switch: <?php echo htmlspecialchars($resultAlternative); ?></p>
+    <?php } ?>
+
+    <hr>
+    <p><?php echo htmlspecialchars($group); ?></p>
+    <p><?php echo htmlspecialchars($developer); ?></p>
+    <p><?php echo htmlspecialchars($created); ?></p>
+    <p>Поточна дата: <?php echo htmlspecialchars($now); ?></p>
+</body>
+</html>
